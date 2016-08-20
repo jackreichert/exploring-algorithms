0b3a32c3f7a9be2a9f6bd29953a41c44fe05ed04
Jack Reichert <awesome@jackreichert.com>
2016-08-20 14:07:56 -0400

    insertion sort, first test

--- js/src/InsertionSort.js
+++ js/src/InsertionSort.js
@@ -1,20 +1,6 @@
 'use strict';
 class InsertionSort {
-  sort(arr){
-    var newArr = [];
-    for(var i = 0; i < arr.length; i++){
-      newArr = this.insertIntoPlace(newArr, arr[i]);
-    }
-    return newArr;
-  }
-  insertIntoPlace(arr, newVal){
-    for( var i = 0; i < arr.length; i++){
-      if (undefined === typeof arr[i] || arr[i] > newVal){
-        arr.splice(i,0,newVal);
-        return arr;
-      }
-    }
-    arr.push(newVal);
-    return arr;
+  insertIntoPlace(arr, val){
+    return arr.push(val);
   }
 }
--- js/tests/testingInsertionSort.js
+++ js/tests/testingInsertionSort.js
@@ -5,32 +5,7 @@ QUnit.test("New insertion search init", function (assert) {
   assert.ok(undefined !== insertionSort, 'Passed!');
 });
 
-QUnit.test("When one is given, one is returned", function (assert) {
+QUnit.test("When 1 is given, insertIntoPlace returns 1", function (assert) {
   var insertionSort = new InsertionSort();
-  assert.ok( '1' === insertionSort.sort([1]).toString(), 'Passed!');
-});
-
-QUnit.test("When 1,2 is given, 1,2 is returned", function (assert) {
-  var insertionSort = new InsertionSort();
-  assert.ok( '1,2' === insertionSort.sort([1,2]).toString(), 'Passed!');
-});
-
-QUnit.test("When 2,1 is given, 1,2 is returned", function (assert) {
-  var insertionSort = new InsertionSort();
-  assert.ok( '1,2' === insertionSort.sort([2,1]).toString(), 'Passed!');
-});
-
-QUnit.test("When 1,2,3 is given, 1,2,3 is returned", function (assert) {
-  var insertionSort = new InsertionSort();
-  assert.ok( '1,2,3' === insertionSort.sort([1,2,3]).toString(), 'Passed!');
-});
-
-QUnit.test("When 3,1,2 is given, 1,2,3 is returned", function (assert) {
-  var insertionSort = new InsertionSort();
-  assert.ok( '1,2,3' === insertionSort.sort([3,1,2]).toString(), 'Passed!');
-});
-
-QUnit.test("When 3,9,32,34543,3,3,23,1,2,4 is given, 1,2,3,3,3,4,9,32,34543 is returned", function (assert) {
-  var insertionSort = new InsertionSort();
-  assert.ok( '1,2,3,3,3,4,9,23,32,34543' === insertionSort.sort([3,9,32,34543,3,3,23,1,2,4]).toString(), 'Passed!');
+  assert.ok('1' === insertionSort.insertIntoPlace([], 1).toString());
 });

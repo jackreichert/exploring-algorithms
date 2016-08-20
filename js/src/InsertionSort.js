c05081fb40e88a660c0b8d5e847a2c00cf96cfb1
Jack Reichert <awesome@jackreichert.com>
2016-08-20 14:15:14 -0400

    insertion sort, third test

--- js/src/InsertionSort.js
+++ js/src/InsertionSort.js
@@ -1,7 +1,13 @@
 'use strict';
 class InsertionSort {
   insertIntoPlace(arr, val){
-    arr.unshift(val);
+    for( var i = 0; i < arr.length; i++ ){
+      if (val < arr[i]){
+        arr.splice(i,0,val);
+        return arr;
+      }
+    }
+    arr.push(val);
     return arr;
   }
 }
--- js/tests/testingInsertionSort.js
+++ js/tests/testingInsertionSort.js
@@ -12,5 +12,11 @@ QUnit.test("When 1 is given, insertIntoPlace returns 1", function (assert) {
 
 QUnit.test("When 2 is given to 3,4, insertIntoPlace returns 2,3,4", function (assert) {
   var insertionSort = new InsertionSort();
+  console.log(insertionSort.insertIntoPlace([3,4], 2).toString());
   assert.ok('2,3,4' === insertionSort.insertIntoPlace([3,4], 2).toString());
+});
+
+QUnit.test("When 3 is given to 2,7,4, insertIntoPlace returns 2,3,7,4", function (assert) {
+  var insertionSort = new InsertionSort();
+  assert.ok('2,3,7,4' === insertionSort.insertIntoPlace([2,7,4], 3).toString());
 });

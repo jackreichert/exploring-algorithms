4aeb5f4d4f652fcdef7c75e515a884ddc3f43a2c
Jack Reichert <awesome@jackreichert.com>
2017-04-22 18:01:26 -0400

    misc

---
+++ BinaryTree/py/BinaryTree.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+
+class BinaryTree:
+    def __init__(self):
+        pass
+
+    def getLeft(self):
+        pass
+
+    def getRight(self):
+        pass
+
+    def getValue(self):
+        pass
